8ec7694acb13f4eb18cdb5e46368135f64575e81
dai <hiradigno@gmail.com>
2018-08-11 17:10:23 +0900

    initial commit

---
+++ my-app/src/components/NumSeparator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form action="">
+      <input type="text" v-model.number="from">から
+      <input type="text" v-model.number="to">までをカンマ区切りで出力
+      <br>
+      <textarea readonly cols="30" rows="10" v-model="separatedNum"></textarea>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "NumSeparator",
+    data: function() {
+      return {
+        from: '',
+        to: ''
+      }
+    },
+    computed: {
+      separatedNum: function() {
+        if (this.from !== '' && this.to !== '') {
+          let ar = []
+          for (let i = this.from; i <= this.to; i++) {
+            ar.push(i)
+          }
+          return ar.join()
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
